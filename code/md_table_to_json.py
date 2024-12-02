61e128684c5cc1691247b2bb1c48629d5fd69748
kaktusztea <kaktusztea@protonmail.com>
2024-12-02 19:28:13 +0100

    prototype: code/md_table_to_json.py IV

--- code/md_table_to_json.py
+++ code/md_table_to_json.py
@@ -3,8 +3,11 @@
 
 # https://stackoverflow.com/questions/66185838/convert-markdown-table-to-json-with-python
 
+from pathlib import Path
 import json
+import sys
 import os
+import re
 
 class MdToJsonConverter:
 
@@ -14,13 +17,36 @@ class MdToJsonConverter:
         self.raw_md = None
         self.raw_json = []
 
+        self.tag_start = '<!-- tag: md_table_start -->'
+        self.tag_end = '<!-- tag: md_table_end -->'
+        self.filter_out_chars = ['**', '`']
+
         self.read_md()
         self.filter_raw_md()
+        self.get_table_sections_from_raw_md()
         self.convert_md_to_json()
+        # self.order_json_by_fegyver_name()         # TODO: kell ez?
 
     def read_md(self):
         with open(path_md, 'r') as fh:
             self.raw_md = fh.read()
+
+    def filter_raw_md(self):
+        """
+        Filter unnecessary strings from raw markdown data
+        """
+        for ch in self.filter_out_chars:
+            self.raw_md = self.raw_md.replace(ch, '')
+
+    def get_table_sections_from_raw_md(self):
+        """
+        Filter markdown table code from raw markdown data
+        """
+        blocks = re.split(rf'{self.tag_start}|{self.tag_end}', self.raw_md)
+        blocks = [block.strip() for block in blocks if block.strip()]
+        self.raw_md = [block for block in blocks if block != self.tag_start]
+        # TODO: handle multiple sections
+
 
     def convert_md_to_json(self):
         for n, line in enumerate(self.raw_md[1:-1].split('\n')):
@@ -34,6 +60,13 @@ class MdToJsonConverter:
                 self.raw_json.append(data)
             n += 1
 
+    def order_json_by_fegyver_name(self):
+        """"
+        Order json by fegyver nev
+        """
+        pass
+        # TODO: kell ez?? Lehet, hogy jó a meglevő sorrend
+
     def write_json(self, path_json=None):
         if not path_json:
             path_json = self.path_json
@@ -43,41 +76,46 @@ class MdToJsonConverter:
     def get_json_data(self):
         return self.raw_json
 
-    def filter_raw_md():
-        """
-        Filter markdown table code from raw markdown data
-        """
-        pass
-        # TODO: filter md table from "self.raw_md"
 
-
-def write_json(path_json, raw_json):
+def write_out_to_json(path_json, raw_json):
     with open(path_json, 'w', encoding="utf-8") as fj:
         fj.seek(0)
         fj.truncate()
         json.dump(raw_json, fj, ensure_ascii=False, indent=4)
 
 
-dir_code = os.path.dirname(os.path.abspath(__file__))
-dir_md_root = os.path.join(dir_code, '../md')
 
-fegyver_raw_md_files = ['068_02_kozelharci_fegyverek.md',
-                 '068_03_kardvivo_fegyverek.md',
-                 '068_04_zuzo_fegyverek.md',
-                 '068_05_landzsavivo_fegyverek.md']
+if __name__ == "__main__":
 
-
-raw_full_json = []
-for fegyver_file in fegyver_raw_md_files:
-    path_md = os.path.join(dir_md_root, fegyver_file)
-    mjc = MdToJsonConverter(path_md, None)
-    raw_full_json.append(mjc.get_json_data())
+    dir_code = os.path.dirname(os.path.abspath(__file__))
+    dir_data_root = os.path.join(Path(dir_code).parent, 'data')
+    dir_md_root = os.path.join(Path(dir_code).parent, 'md')
 
 
-path_json="/repo/github/km100.code/data/fegyverek.json"
-write_json(path_json, raw_full_json)
+    fegyver_raw_md_files = [f for f in os.listdir(dir_md_root) if re.match(r'[a-z]*_fegyverek*\.md', f)]
+
+    print(dir_data_root)
+    print(dir_md_root)
+    print(fegyver_raw_md_files)
+
+    sys.exit(0)
+
+    # fegyver_raw_md_files = ['068_02_kozelharci_fegyverek.md',
+    #                 '068_03_kardvivo_fegyverek.md',
+    #                 '068_04_zuzo_fegyverek.md',
+    #                 '068_05_landzsavivo_fegyverek.md']
+
+    full_json = []
+    for fegyver_file in fegyver_raw_md_files:
+        path_md = os.path.join(dir_md_root, fegyver_file)
+        mjc = MdToJsonConverter(path_md, None)
+        full_json.append(mjc.get_json_data())
 
 
-# mjc = MdToJsonConverter(path_md, path_json)
-# mjc.write_json()
+    path_json=os.path.join(dir_data_root, "fegyverek.json")
+    write_out_to_json(path_json, full_json)
+
+
+    # mjc = MdToJsonConverter(path_md, path_json)
+    # mjc.write_json()
 
