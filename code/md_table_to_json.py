e14eac15a32e4874e94a7451be5b9c21c68711eb
kaktusztea <kaktusztea@protonmail.com>
2024-12-03 23:23:49 +0100

    code/md_table_to_json.py XIV

--- code/md_table_to_json.py
+++ code/md_table_to_json.py
@@ -18,17 +18,19 @@ class MdToJsonConverter:
         self.raw_json = []
         self.filepattern = params['file_pattern']
         self.table_pattern = params['table_pattern']
+        self.sortkey = params['sortkey']
 
         self.tag_start = f"<!-- tag: md_table_{self.table_pattern}_start -->"
         self.tag_end = f"<!-- tag: md_table_{self.table_pattern}_end -->"
         self.filter_out_chars = ['**', '`', '⭕TODO⭕', '⭕TODO', '⭕']
         self.skip_columns = params['skip_columns']
 
+        # execute processing
         self.read_md()
         self.filter_raw_md()
         self.get_table_sections_from_raw_md()
         self.convert_md_to_json()
-        # self.order_json_by_fegyver_name()
+        self.order_json_by_key()
 
     def read_md(self):
         with open(path_md, 'r') as fh:
@@ -72,11 +74,11 @@ class MdToJsonConverter:
                 self.raw_json.append(data)
             n += 1
 
-    def order_json_by_fegyver_name(self):
+    def order_json_by_key(self):
         """"
         Order json by fegyver nev
         """
-        pass
+        self.raw_json = sorted(self.raw_json, key=lambda x: x[self.sortkey].lower())
 
     def write_json(self, path_json=None):
         if not path_json:
@@ -86,6 +88,12 @@ class MdToJsonConverter:
 
     def get_json_data(self):
         return self.raw_json
+
+def order_list_of_dicts_by_key(raw_json, sortkey):
+    """"
+    Order list of dicts  by key
+    """
+    return sorted(raw_json, key=lambda x: x[sortkey].lower())
 
 
 def write_out_to_json(path_json, raw_json):
@@ -108,6 +116,7 @@ if __name__ == "__main__":
         'output':'fegyverek.json',
         'file_pattern': 'fegyverek.md',
         'table_pattern': 'fegyver',
+        'sortkey': 'Fegyver',
         'skip_columns': []
        },
        {
@@ -115,6 +124,7 @@ if __name__ == "__main__":
         'output':'kepzettseg_kp.json',
         'file_pattern': 'kepzettsegszintek_kp_igenye.md',
         'table_pattern': 'kepzettsegkp',
+        'sortkey': 'Képzettség Szint',
         'skip_columns': ['Fokozat']
        },
     ]
@@ -127,5 +137,7 @@ if __name__ == "__main__":
                 path_md = os.path.join(dir_md, fname)
                 mjc = MdToJsonConverter(path_md, None, d)
                 full_json.extend(mjc.get_json_data())
+        # Sort the list by sortkey
+        full_json = order_list_of_dicts_by_key(full_json, d['sortkey'])
         path_json=os.path.join(dir_data, d['output'])
         write_out_to_json(path_json, full_json)
