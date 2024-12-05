8678e7cb6907d9fe23d5f7067fe18fa3002ac799
kaktusztea <kaktusztea@protonmail.com>
2024-12-05 16:47:46 +0100

    new: util.py with common defs

---
+++ code/lib/util.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import json
+
+def order_list_of_dicts_by_key(list_dicts, sortkey):
+    """"
+    Order list of dicts  by key
+    """
+    return sorted(list_dicts, key=lambda x: x[sortkey].lower())
+
+
+def write_out_to_json(path_json, list_dicts):
+    with open(path_json, 'w', encoding="utf-8") as fj:
+        fj.seek(0)
+        fj.truncate()
+        json.dump(list_dicts, fj, ensure_ascii=False, indent=4)
